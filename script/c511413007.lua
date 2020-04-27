b626d28c1b2c0f6c6a5536b78da0cac045c643d0
IFDexxus <thathomestuckguy@gmail.com>
2020-04-27 15:45:18 +0100

    M

--- script/c511413007.lua
+++ script/c511413007.lua
@@ -1,22 +1,35 @@
 --最強の盾
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsDefensePos))
+	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsDefensePos),nil,nil,nil,s.op)
 	--atk/def
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(s.atkval)
+	e2:SetValue(1)
+	e2:SetTarget(s.seqtg)
+	e2:SetOperation(s.activate)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_EQUIP)
 	e3:SetCode(EFFECT_UPDATE_DEFENSE)
 	e3:SetValue(s.defval)
 	c:RegisterEffect(e3)
-end
-function s.atkval(e,c)
-	if c:IsDefensePos() then return 0 else return c:GetBaseDefense() end
-end
+end	
 function s.defval(e,c)
-	if c:IsAttackPos() then return 0 else return c:GetBaseAttack() end
+	return 700
+end
+function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+	local seq=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
+	Duel.Hint(HINT_ZONE,tp,seq)
+	e:SetLabel(math.log(seq,2))
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        Duel.RaiseSingleEvent(c,id,e,0,0,0,0,0)
+    end
 end
