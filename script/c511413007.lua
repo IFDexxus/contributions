003399de2029db35b491257ac1b294124161278e
IFDexxus <thathomestuckguy@gmail.com>
2020-04-27 13:49:44 +0100

    Chaos Shield equip condition

--- script/c511413007.lua
+++ script/c511413007.lua
@@ -1,7 +1,7 @@
 --最強の盾
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsRace,RACE_WARRIOR))
+	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsDefensePos))
 	--atk/def
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
